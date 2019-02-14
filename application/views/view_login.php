1b38543061519bc221ef1b27fc2c2f4b97d7eb35
Yuliana-Minu <karen_palaciomi@fet.edu.co>
2019-02-13 20:22:23 -0500

    Vista Login

---
+++ application/views/view_login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body style="align:center;">
+    <div class="">
+      <div class="">
+        <img src="<?php echo base_url(); ?>images/login.png" alt="" width="100px;" height="100px;">
+      </div>
+      <label>Usuario</label>
+      <input type="text" name="nombre_usuario"  id="nombre_usuario" value="">
+      <br>
+      <label>Contraseña</label>
+      <input type="password" name="password_usuario" id="password_usuario" value="">
+      <br>
+      <button type="button" name="button">Login</button>
+    </div>
+  </body>
+</html>
+<style media="screen">
+
+</style>
