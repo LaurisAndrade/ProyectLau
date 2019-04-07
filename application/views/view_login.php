1f963cddf7930b2664c98a37d8e4004a5d6067d3
Maria Jose <mjcarvajal2000@gmail.com>
2019-04-07 08:24:53 -0500

    e

--- application/views/view_login.php
+++ application/views/view_login.php
@@ -18,8 +18,8 @@
         </div>
 
       <div data-role="main">
-        <form class="" align="center" action="<?php base_url(); ?>index.php/login/valida_user" method="post" data-ajax="false">
-          <div class="login" align="center" style="width:700px;height:300px;float:center" >
+        <form class="" align="center" action="<?php base_url(); ?>index.php/login/valida_user" method="post" data-ajax="false" style="border:none;background-color: transparent;">
+          <div class="login" align="center" style="width:700px;height:300px;float:center;border:none;" >
 
 
           <div data-role="fieldcontain" align="center" style="width:300px;height:200px;float:right;">
@@ -30,34 +30,25 @@
               <input type="password" name="password_usuario" id="password_usuario" value="" placeholder="Contraseña" required />
 
               <button type="submit" name="button"  >Ingresar</button>
+
+              <div data-role="main">
+                <form class="" action="<?php echo base_url(); ?>index.php/registrarse/form_registro" method="post" data-ajax="false">
+                  <div data-role="fieldcontain" align="center" style="width:300px;height:200px;float:right;border:none;">
+                  <button type="submit" name="button" >Registrarse</button>
+                </div>
+                </form>
+            </div>
           </div>
 
 
             </div>
         </form>
       </div>
-        <div data-role="main">
-          <div data-role="main">
-            <form class="" action="<?php echo base_url(); ?>index.php/registrarse/form_registro" method="post" data-ajax="false">
-              <div data-role="fieldcontain" align="center" style="width:300px;height:200px;float:right;">
-              <button type="submit" name="button" >Registrarse</button>
-            </div>
-            </form>
-
-
-        </div>
-        <div data-role="footer">
-
-
-
-        </div>
-      </div>
-
 
       <div data-role="footer">
 
       </div>
-    </div>
+
 
 
   </body>
