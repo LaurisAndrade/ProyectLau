24e9f609f0de1daea6bb8aa7ed7663da4a345d99
Yuliana-Minu <karen_palaciomi@fet.edu.co>
2019-04-06 19:15:22 -0500

    Update view_registro.php

---
+++ application/views/view_registro.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Registro</title>
+    <link rel="stylesheet" href="<?php echo base_url(); ?>jquery.mobile/jquery.mobile-1.4.5.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>jquery.mobile/jquery.mobile.icons-1.4.5.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>jquery.mobile/jquery.mobile.structure-1.4.5.min.css">
+    <script src="<?php echo base_url(); ?>jquery.mobile/jquery.js"></script>
+    <script src="<?php echo base_url(); ?>jquery.mobile/jquery.mobile-1.4.5.js"></script>
+  </head>
+  <body style="align:center;">
+    <div class="">
+      <div class="">
+        <img src="<?php echo base_url(); ?>images/login.png" alt="" width="100px;" height="100px;">
+      </div>
+      <label>Usuario</label>
+      <input type="text" name="nombre_usuario"  id="nombre_usuario" value="">
+      <br>
+      <label>Contraseña</label>
+      <input type="password" name="password_usuario" id="password_usuario" value="">
+      <br>
+      <button type="button" name="button">Login</button>
+    </div>
+  </body>
+</html>
+<style media="screen">
+
+</style>
