d1315ab999718a6a297535ed6b88bf3f26fa10e7
Yuliana-Minu <karen_palaciomi@fet.edu.co>
2019-04-07 17:07:39 -0500

    9

--- application/views/view_principal.php
+++ application/views/view_principal.php
@@ -14,41 +14,27 @@
 
   <body>
 
-    <div data_role="page" id="pricipals">
-      <div data-role="header">
-        <a href="index.php"  data-icon="home">Inicio</a>
-        <img src="images/nombre.jpg" alt="" style="width:1525px; height:200px;">
-        <a href="#login"  data-icon="grid">Login</a>
-        <div data-role="navbar">
-          <ul>
-            <li><a href="#page2">Acerca</a></li>
-            <li><a href="#">Productos</a></li>
-            <li><a href="#">Sucursales</a></li>
-            <li><a href="#">Contactos</a></li>
-          </ul>
-        </div>
-      </div>
-
-      <div data-role="footer">
-      </div>
-    </div>
-
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
     <div data-role="page" id="page1">
       <div data-role="header">
         <a href="index.php"  data-icon="home">Inicio</a>
         <img src="images/nombre.jpg" alt="" style="width:1525px; height:200px;">
-        <a href="#login"  data-icon="grid">Login</a>
+        <a href="<?php echo base_url(); ?>index.php/login/see_login"  data-icon="grid">Login</a>
         <div data-role="navbar">
         </div>
       </div>
 
+      <br>
+      <br>
+
+
       <div data-role="main">
         <div class="head">
           <center><h1>¿Que es Energetic Body Gym?</h1>
-          <h2><p>Nuestro aplicativo permite agilizar,validar, controlar y realizar seguimientos a las actividades</p>
-              <p>relacionadas con el gimnasio y las actividades deportivas organizadas por el area de bienestar,</p>
-              <p>motivate a participar de ellos, el esfuerzo de hoy es el exito mañana,participa de nuestras actividades</p>
-              <p>deportivas...no olvides escribirte a ellas.</p></h2></center>
+          <h3><p>Nuestro aplicativo permite agilizar,validar, controlar y realizar seguimientos a las actividades</p>
+              <p>relacionadas con el gimnasio y las actividades deportivas organizadas por el area de</p>
+              <p> bienestar, motivate a participar de ellos, el esfuerzo de hoy es el exito mañana,</p>
+              <p>participa de nuestras actividades deportivas...no olvides escribirte a ellas.</p></h3></center>
         </div>
 
       </div>
@@ -60,19 +46,48 @@
     <br>
     <br>
     <br>
-    <br>
-    <br>
+
     <br><br>
     <div class="ui-content">
     <div class="ui-grid-a">
-        <div class="ui-block-a"><div class="ui-bar ui-bar-a" style="height:400px"><img src="images/bienvenido.jpg" alt="" style="width:720px; height:400px;"></div></div>
-        <div class="ui-block-b"><div class="ui-bar ui-bar-a" style="height:400px"><img src="images/ejerci.jpg" alt="" style="width:720px; height:400px;"></div></div>
+        <div class="ui-block-a"><div class="ui-bar ui-bar-a" style="height:300px; width:400px;"><img src="images/bienvenido.jpg" alt="" style="width:400px; height:300px;"></div></div>
+        <div class="ui-block-b"><div class="ui-bar ui-bar-a" style="height:300px; width:400px;"><img src="images/ejerci.jpg" alt="" style="width:400px; height:300px;"></div></div>
 
 
     </div><!-- /grid-a -->
 
 
     </div>
+    <br>
+    <br>
+    <br>
+    <div data-role="main">
+      <div class="head">
+        <center><h1>¿Porque debes entrar a nuestro Gimnasio?</h1>
+        <h3><p>Energetic Body Gym cuenta con un equipo de profesionales en el área deportiva, el cual</p>
+            <p>es una gran ventaja ya que cada persona que entre a ser parte de nuestro gimnasio se</p>
+            <p>evaluara su condición física para luego si asignar su adecuada rutina e instructor.</p>
+            <p>Que esperas para inscribirte...</p></h3></center>
+      </div>
 
+
+    </div>
+
+    <br>
+    <br>
+    <br>
+
+    <div data-role="footer">
+      <div class="footer" style="width:1400px; height:170px; background-color:black;">
+
+<br>
+        <center><h2>Energetic Body Gym</h2>
+        <p>Contáctenos: 3143219894-3132956425</p>
+        <p>Email: energetic_gym@gmail.com</p></center>
+
+
+      </div>
+      <!--<img src="images/Rutinas/banner.png" alt="" >-->
+    </div>
   </body>
 </html>
