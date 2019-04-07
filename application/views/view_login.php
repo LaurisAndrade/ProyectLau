7e7867c99623aa7847d072dff7cb568b4f5628ed
Maria Jose <mjcarvajal2000@gmail.com>
2019-04-06 20:27:59 -0500

    e

--- application/views/view_login.php
+++ application/views/view_login.php
@@ -1,29 +1,60 @@
 <!DOCTYPE html>
-<html lang="es" dir="ltr">
+<html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Energit</title>
+    <title></title>
     <link rel="stylesheet" href="<?php base_url(); ?>jquery.mobile/jquery.mobile-1.4.5.min.css">
     <link rel="stylesheet" href="<?php base_url(); ?>jquery.mobile/jquery.mobile.icons-1.4.5.min.css">
     <link rel="stylesheet" href="<?php base_url(); ?>jquery.mobile/jquery.mobile.structure-1.4.5.min.css">
     <script src="<?php base_url(); ?>jquery.mobile/jquery.js"></script>
     <script src="<?php base_url(); ?>jquery.mobile/jquery.mobile-1.4.5.js"></script>
+
   </head>
-  <body style="align:center;">
-    <div class="">
-      <div class="">
-        <img src="<?php echo base_url(); ?>images/login.png" alt="" width="100px;" height="100px;">
+  <body>
+    <div data-role="page" id="page1">
+      <div data-role="header">
+        <a href="<?php echo base_url(); ?>index.php/login/index" data-icon="home" data-ajax='false'>Inicio</a>
+        <h1><img src="images/icono.png" alt="" style="width:100px;height:50px;"></h1>
+        <a href="#"  data-icon="grid">Login</a>
+        </div>
+
+      <div data-role="main">
+        <form class="" action="<?php base_url(); ?>index.php/login/valida_user" method="post" data-ajax="false">
+          <div data-role="fieldcontain">
+              <label for="name">Usuario</label>
+              <input type="text" name="nombre_usuario"  id="nombre_usuario" value="" placeholder="Usuario" required />
+      <br>
+              <label for="name">Usuario</label>
+              <input type="password" name="password_usuario" id="password_usuario" value="" placeholder="Contraseña" required />
+
+          </div>
+
+          <button type="submit" name="button" >Ingresar</button>
+
+        </form>
+        <div data-role="main">
+          <div data-role="main">
+            <form class="" action="<?php echo base_url(); ?>index.php/operaciones/valida_operaciones" method="post" data-ajax="false">
+
+              <button type="submit" name="button" >Registrarse</button>
+
+            </form>
+
+
+        </div>
+        <div data-role="footer">
+
+
+
+        </div>
       </div>
-      <label>Usuario</label>
-      <input type="text" name="nombre_usuario"  id="nombre_usuario" value="" placeholder="Usuario" required>
-      <br>
-      <label>Contraseña</label>
-      <input type="password" name="password_usuario" id="password_usuario" value="" placeholder="Contraseña" required>
-      <br>
-      <button type="button" name="button">Login</button>
+
+      </div>
+      <div data-role="footer">
+
+      </div>
     </div>
+
+
   </body>
 </html>
-<style media="screen">
-
-</style>
